a0ae01e9e3a7cab807bc03defa1d0935066d1e69
Tural Ismayilov <analyticsengineer17@gmail.com>
2024-04-07 16:49:08 +0000

    Add first macro

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,7 +4,7 @@ with payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        (amount/100) as amount,
+        {{ cents_to_dollars('amount', 4) }} as amount,
         created as created_at,
         _batched_at
     from {{ source('stripe', 'payment') }}

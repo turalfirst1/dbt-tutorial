c16c9b11d125e1f72cd36922854758073e06a091
John Hopkins <analyticsengineer17@gmail.com>
2024-04-12 13:40:12 +0000

    random fixes

--- models/staging/jaffle_shop/customer_daily_summary.sql
+++
@@ -1,8 +0,0 @@
-select
-
-    {{ dbt_utils.generate_surrogate_key(['customer_id', 'order_date']) }} as id,
-    customer_id,
-    order_date,
-    count(*)
-from {{ ref('stg_orders') }}
-group by 1,2,3
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,7 +4,7 @@ with payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        {{ cents_to_dollars('amount', 4) }} as amount,
+        amount / 100 as amount,
         created as created_at,
         _batched_at
     from {{ source('stripe', 'payment') }}
